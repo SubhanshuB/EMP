6a3979499dd3672718a27ca976665d68da76085b
SubhanshuB <30664033+SubhanshuB@users.noreply.github.com>
2018-11-20 00:48:10 +0530

    Update Home.java

--- src/Home.java
+++ src/Home.java
@@ -118,7 +118,8 @@ public class Home {
 			    }
 			    });
 
-		JLabel logo = new JLabel("",new ImageIcon("C:\\Users\\Tanishk Pokhariya\\Downloads\\EVM-master\\EVM-master\\src\\ecj.jpg"),JLabel.CENTER);
+		Here I made a change
+		 JLabel logo = new JLabel("",new ImageIcon("C:\\Users\\Tanishk Pokhariya\\Downloads\\EVM-master\\EVM-master\\src\\ecj.jpg"),JLabel.CENTER);
 		logo.setBounds(10, 12, 70, 70);
 		f.getContentPane().add(logo);
 		f.setVisible(true);
