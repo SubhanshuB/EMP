d46f26fdd0f0b21c6562a02b8b0e67238e3dfe48
SubhanshuB <30664033+SubhanshuB@users.noreply.github.com>
2018-11-28 00:58:04 +0530

    Result Page Added

---
+++ src/Result.java
@@ -0,0 +1,28 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.JTable;
+import javax.swing.JProgressBar;
+public class Result {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JFrame res=new JFrame();
+		res.setVisible(true);
+		res.setSize(1080,720);
+		res.getContentPane().setLayout(null);
+		
+		String data[][]={{"10","20","45","454","23","12"}}; 
+		String column[]= {"BJP","CONGRESS","AAP","BSP","RJD","NOTA"};
+	}
+}
