bc783592e2ab5bd2ad37532137ff9c61b58f0f97
SubhanshuB <30664033+SubhanshuB@users.noreply.github.com>
2018-12-06 00:46:57 +0530

    Update Result.java

--- src/Result.java
+++ src/Result.java
@@ -22,7 +22,33 @@ public class Result {
 		res.setSize(1080,720);
 		res.getContentPane().setLayout(null);
 		
+		JLabel lblBjp = new JLabel("BJP");
+		lblBjp.setFont(new Font("Dialog", Font.BOLD, 20));
+		lblBjp.setBounds(156, 165, 38, 26);
+		res.getContentPane().add(lblBjp);
+		
+		JLabel label = new JLabel("Congress");
+		label.setFont(new Font("Dialog", Font.BOLD, 20));
+		label.setBounds(299, 165, 91, 26);
+		res.getContentPane().add(label);
+		
+		JLabel lblBjp_1 = new JLabel("BJP");
+		lblBjp_1.setFont(new Font("Dialog", Font.BOLD, 20));
+		lblBjp_1.setBounds(464, 165, 38, 26);
+		res.getContentPane().add(lblBjp_1);
+		
+		JLabel label_2 = new JLabel("BJP");
+		label_2.setFont(new Font("Dialog", Font.BOLD, 20));
+		label_2.setBounds(657, 165, 38, 26);
+		res.getContentPane().add(label_2);
+		
+		JLabel label_3 = new JLabel("BJP");
+		label_3.setFont(new Font("Dialog", Font.BOLD, 20));
+		label_3.setBounds(826, 165, 38, 26);
+		res.getContentPane().add(label_3);
+		
 		String data[][]={{"10","20","45","454","23","12"}}; 
 		String column[]= {"BJP","CONGRESS","AAP","BSP","RJD","NOTA"};
+		
 	}
 }
